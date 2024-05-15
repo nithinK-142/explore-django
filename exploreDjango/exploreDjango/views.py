37928a5ee6c6334e417d97fce156481efc22a591
nithinK-142 <nithin.sagar.359@gmail.com>
2024-05-15 19:46:08 +0530

    add: templates.

--- exploreDjango/exploreDjango/views.py
+++ exploreDjango/exploreDjango/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("home")
+    return render(request, "website/index.html")
 
 def about(request):
     return HttpResponse("about")
